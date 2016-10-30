0bf633e1fd00e26ad247e42994c7294797a1233b
RonyVidaur <rony.vidaur@gmail.com>
2016-10-30 12:16:55 -0600

    project structure & dependencies updated

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import TodosList from './todos-list'
+
+const todos = [
+  {
+    task: 'take a shower',
+    isCompleted: false
+  },
+  {
+    task: 'Be awesome',
+    isCompleted: true
+  }
+]
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos
+    }
+  }
   render() {
     return (
       <div>
-        <h1>Well Hello!</h1>
+        <h1>Well Hello! these are your pending tasks</h1>
+        <h2>Get stuff done!</h2>
+        <TodosList todos={this.state.todos} />
       </div>
     )
   }
